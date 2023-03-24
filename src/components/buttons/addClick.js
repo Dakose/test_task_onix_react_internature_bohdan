cc0ddd518023c1c76b3b9e299b8bf4dbabe5a839
Bohdan Holubnychyi <bohdanholubn@gmail.com>
2023-03-24 23:00:30 +0200

    remove click

--- src/components/buttons/addClick.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-handleAddClick = () => {
-  const newFate = { name: "", dateOfBirth: "", ... };
-  this.setState(prevState => {
-    const newFates = prevState.fates.concat(newFate);
-    return { fates: newFates };
-  });
-};
--- src/components/buttons/removeClick.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-handleRemoveClick = (index) => {
-  this.setState(prevState => {
-    const newFates = [...prevState.fates].filter((fate, i) => i !== index);
-    return { fates: newFates };
-  });
-};
